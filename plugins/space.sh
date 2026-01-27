69b84c2384039b140f4ff1727497bb1e87103416
BosEriko <bos@eriko.ph>
2026-01-27 21:52:10 +0800

    add unique design to sketchybar

--- plugins/space.sh
+++ plugins/space.sh
@@ -4,4 +4,19 @@
 # the space invoking this script (with name: $NAME) is currently selected:
 # https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
 
-sketchybar --set "$NAME" background.drawing="$SELECTED"
+WHITE=0xFFBCBCBC
+YELLOW=0xFFFFDF20
+
+if [ "$SELECTED" = "true" ]; then
+  sketchybar --set "$NAME" \
+    background.drawing=on \
+    background.color=0xEB1e1e2e \
+    icon.color=$YELLOW \
+    background.border_color=$YELLOW
+else
+  sketchybar --set "$NAME" \
+    background.drawing=on \
+    background.color=0xEB1e1e2e \
+    icon.color=$WHITE \
+    background.border_color=$WHITE
+fi
