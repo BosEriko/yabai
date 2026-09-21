7945f9d795f9c86a52c82eece8feeec5d1e2407a
BosEriko <bos@eriko.ph>
2026-09-21 17:29:55 +0800

    ✨ Add percentage for storage

--- plugins/storage.sh
+++ plugins/storage.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
-read -r SIZE USED AVAIL <<< "$(df -H / | awk 'NR==2 {print $2, $3, $4}')"
+case "$SENDER" in
+  mouse.entered)
+    sketchybar --set "$NAME" popup.drawing=on
+    exit 0
+    ;;
+  mouse.exited|mouse.exited.global)
+    sketchybar --set "$NAME" popup.drawing=off
+    exit 0
+    ;;
+esac
 
-sketchybar --set "$NAME" label="$AVAIL/$SIZE"
+read -r SIZE USED AVAIL CAPACITY <<< "$(df -H / | awk 'NR==2 {print $2, $3, $4, $5}')"
+
+REMAIN=${CAPACITY%\%}
+REMAIN=$((100 - REMAIN))
+
+sketchybar --set "$NAME" label="${REMAIN}%" \
+  --set "${NAME}.details" label="${AVAIL}/${SIZE}"
