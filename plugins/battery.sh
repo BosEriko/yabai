afc7d8a7e485016966cdcafb69a27c41d8bfcff0
BosEriko <bos@eriko.ph>
2026-09-21 17:35:44 +0800

    ✨ Add indicator when battery is properly charged

--- plugins/battery.sh
+++ plugins/battery.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
+WHITE=0xFFBCBCBC
+GREEN=0xFF34A853
+
 BATT="$(pmset -g batt)"
 PERCENTAGE="$(printf '%s\n' "$BATT" | grep -Eo "[0-9]+%" | head -1 | cut -d% -f1)"
 
 if [ -z "$PERCENTAGE" ] || ! printf '%s\n' "$BATT" | grep -q "discharging"; then
-  sketchybar --set "$NAME" drawing=off --set cpu drawing=on
+  if [ -n "$PERCENTAGE" ] && [ "$PERCENTAGE" -gt 90 ] 2>/dev/null; then
+    CPU_BORDER=$GREEN
+  else
+    CPU_BORDER=$WHITE
+  fi
+  sketchybar --set "$NAME" drawing=off --set cpu drawing=on background.border_color="$CPU_BORDER"
   exit 0
 fi
+
+sketchybar --set cpu background.border_color=$WHITE
 
 case "${PERCENTAGE}" in
   9[0-9]|100) ICON=""
